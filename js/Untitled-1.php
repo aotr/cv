76beed2ace0333e98ee44650f11d189decfb62de
Animesh Chakraborty <animesh.aotr@gmail.com>
2018-06-23 11:02:38 +0530

    Add files via upload

---
+++ js/Untitled-1.php
@@ -0,0 +1,87 @@
+
+
+<?php
+
+return [
+
+	'personal_information' => [
+            'name'  => 'Animesh Chakraborty',
+            'email'  => [
+                'Animesh.aotr@gmail.com'
+            ],
+            'phone'  => [
+                '+91-9635591995',
+            ],
+            'social_info' => [
+
+            ],
+            'address'=>[
+                'home'=> [
+                    'Near Forest Office , Hijli Co-Op Society kharagpur-06'
+                ],
+                'home2' =>[
+                    'Near Forest Office , Hijli Co-Op Society kharagpur-06'
+                ],
+            ],
+            'dob'=>[
+                'dd'   =>  '30',
+                'mm'   =>  '09',
+                'yy'   =>  '1992',
+            ],
+            'c_o'=> 'Amal Chakraborty',
+            'languages' => [
+                'BENGALI',
+                'ENGLISH',
+                'HINDI',
+            ],
+    ],
+    'skills' =>[
+        'frameworks'=>[
+            'Laravel', 
+            'Lumen',
+            'WordPress',
+            'Angular Basic',
+            'AngularJS Basic',
+            'jQuery',
+            'Bootstrap',
+        ],
+        'languages'=>[
+            'SWIFT',
+            'PHP',
+            'C',
+            'C++',
+            'JAVA',
+            'C#',
+        ],
+        'front_end_languages'=>[
+            'Storyboard(iPhone)',
+            'HTML', 
+            'JavaScript', 
+            'XML'
+        ],
+        'tools'=>[
+            'Xcode',
+            'VS Code ',
+            'Eclipse',
+            'Atom',
+            'Sublime',
+            'Text',
+            'Brackets',
+            'Notepad++'
+        ],
+        'task_project_management'=>[
+            'Zoho projects', 
+            'Mantis Bug Tracker', 
+            'Bitbucket Basic'
+        ],
+        'methodologies'=>[
+            'Agile'
+
+        ],
+        'other'=>[
+            'NPM', 
+            'Gulp'
+        ],
+    ],
+
+];
---
+++ js/app.js
@@ -0,0 +1,26 @@
+angular.module('aotr', [])
+.controller('personal_information', function($scope, $http) {
+    $http.
+    get(
+        'http://dev.iframeinterface.com/animesh-info',
+        {
+        params:  {},
+        headers: {'Access-Control-Allow-Origin': '*'}
+    }
+    ).
+    then(function(response) {
+                console.info(response);
+                $scope.personal_information = response.data.personal_information;
+                $scope.skills = response.data.skills;
+                $scope.educations = response.data.education;
+            });
+    
+    }).
+    filter('titleCase', function() {
+    return function(input) {
+      input = input || '';
+      input = input.replace(RegExp('_', 'g'),' ');
+
+      return input.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
+    };
+  });
